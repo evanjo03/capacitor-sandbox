9f89e8b63073e72434e2aa72034772d21bb0b807
John Evans <john.wes.evans@gmail.com>
2023-07-16 14:47:57 -0500

    Cap Update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,34 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    onClick() {
+      console.log("here");
+    },
+  },
+});
 </script>
 
 <template>
   <div id="app">
     <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <img
+        alt="Vue logo"
+        class="logo"
+        src="@/assets/logo.svg"
+        width="125"
+        height="125"
+      />
 
       <div class="wrapper">
         <HelloWorld msg="You did it!" />
 
+        <button @click="onClick">Click Me</button>
         <nav>
           <router-link to="/">Home</router-link>
           <router-link to="/about">About</router-link>
